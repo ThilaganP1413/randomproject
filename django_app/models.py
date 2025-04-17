57cf3611f85012a808fa3b7280673247b761d627
ThilaganP1413 <thilaganp1413@gmail.com>
2025-04-17 12:16:47 +0530

    modified:   django_app/models.py 	modified:   manage.py

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    fullname = models.CharField(max_length=200, null=True)
+    regno = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True)
+    balance = models.DecimalField(decimal_places=2,max_digits=7,default=0.0)
+
+
+    def __str__(self):
+        return self.fullname
